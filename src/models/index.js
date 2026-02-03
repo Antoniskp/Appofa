2ec3763c46bcb76793cad80118281a1678d172f1
Antonis <antoniskp@gmail.com>
2026-02-03 23:46:39 +0200

    Revert "Fix test suite: location auth, route ordering, React 19 compatibility"

--- src/models/index.js
+++ src/models/index.js
@@ -1,8 +1,6 @@
 const sequelize = require('../config/database');
 const User = require('./User');
 const Article = require('./Article');
-const Location = require('./Location');
-const LocationLink = require('./LocationLink');
 
 // Define associations
 User.hasMany(Article, {
@@ -15,43 +13,8 @@ Article.belongsTo(User, {
   as: 'author'
 });
 
-// Location self-referential association (hierarchical)
-Location.hasMany(Location, {
-  foreignKey: 'parent_id',
-  as: 'children'
-});
-
-Location.belongsTo(Location, {
-  foreignKey: 'parent_id',
-  as: 'parent'
-});
-
-// User home location
-User.belongsTo(Location, {
-  foreignKey: 'home_location_id',
-  as: 'homeLocation'
-});
-
-Location.hasMany(User, {
-  foreignKey: 'home_location_id',
-  as: 'residents'
-});
-
-// Location links association
-LocationLink.belongsTo(Location, {
-  foreignKey: 'location_id',
-  as: 'location'
-});
-
-Location.hasMany(LocationLink, {
-  foreignKey: 'location_id',
-  as: 'links'
-});
-
 module.exports = {
   sequelize,
   User,
-  Article,
-  Location,
-  LocationLink
+  Article
 };
